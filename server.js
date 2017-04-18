b0ee01ff257a3cfe73a8a925ab7f180452c1f7fc
Josh L <joshua@labue.me>
2017-04-18 15:26:47 -0400

    Heroku Port Fix
    Changed how ports are set up to get the port environment variable

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var players = [];
 app.use(express.static('public'));
